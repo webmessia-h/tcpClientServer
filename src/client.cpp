bf3a55495301854b36c7035783b1dd514ce9dcab
webmessiah <110561823+webmessia-h@users.noreply.github.com>
2024-04-25 00:08:19 +0300

    added comments

--- src/client.cpp
+++ src/client.cpp
@@ -8,7 +8,7 @@ int client_sockfd;
 struct sockaddr_in server_addr;
 sockaddr_in *p_serv_addr = &server_addr;
 
-// Initialize CLIENT and send connection request
+// Initialize client and send connection request
 bool connect_to_server(const char *host, int port) {
   // Create socket
   client_sockfd = create_socket(AF_INET, SOCK_STREAM, 0);
@@ -16,7 +16,6 @@ bool connect_to_server(const char *host, int port) {
   p_serv_addr->sin_family = AF_INET;
   p_serv_addr->sin_port = htons(port);
   p_serv_addr->sin_addr.s_addr = inet_addr(host);
-
   // Try to connect to server
   if (connect(client_sockfd, (struct sockaddr *)p_serv_addr,
               sizeof(*p_serv_addr)) < 0) {
@@ -31,16 +30,10 @@ bool connect_to_server(const char *host, int port) {
             << " on port: " << port << std::endl;
   return true;
 }
+
 // Connect to new port
 bool connect_to_new_port(sockaddr_in *p_serv_addr) {
-  // Try to connect to server
-  int yes = 1;
-  if (setsockopt(client_sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) <
-      0) {
-    std::cerr << "Error setting SO_REUSEADDR" << std::endl;
-    close_socket(client_sockfd);
-    return false;
-  }
+  // reinitialize socket
   close_socket(client_sockfd);
   client_sockfd = create_socket(AF_INET, SOCK_STREAM, 0);
   if (connect(client_sockfd, (struct sockaddr *)p_serv_addr,
@@ -88,34 +81,28 @@ void send_file(int socket_fd, const file_info &FILE) {
   // send file size
   send_data(socket_fd, &FILE.file_size, sizeof(FILE.file_size));
 
-  // send file in 1MB chunks
+  // send file in 50MB chunks
   size_t total_bytes_sent = 0;
-  const size_t CHUNK_SIZE = 1024 * 1024 * 50; // 1MB chunk size
+  const size_t CHUNK_SIZE = 1024 * 1024 * 50; // 50MB chunk size
   while (total_bytes_sent < FILE.file_size) {
     size_t remaining_bytes = FILE.file_size - total_bytes_sent;
     size_t chunk_size =
         (remaining_bytes > CHUNK_SIZE) ? CHUNK_SIZE : remaining_bytes;
-
     size_t bytes_sent =
         send_data(socket_fd, FILE.buffer + total_bytes_sent, chunk_size);
     total_bytes_sent += bytes_sent;
-
     // Check for port change
     if (!get_new_port(socket_fd))
       return;
   }
-
   delete[] FILE.buffer;
 }
 void handle_request(int socket_fd) {
   // get filename
   char filename[1024] = {0};
   receive_data(socket_fd, filename, sizeof(filename));
-  // std::cout << filename << std::endl;
-
   // get new port
   get_new_port(socket_fd);
-
   // write file into buffer
   file_info FILE = read_file(filename);
   if (FILE.buffer == nullptr) {
@@ -123,9 +110,7 @@ void handle_request(int socket_fd) {
               << std::endl;
     return;
   }
-
   send_file(socket_fd, FILE);
-  // delete[] FILE.buffer;
 }
 
 int main(int argc, char *argv[]) {
@@ -137,8 +122,10 @@ int main(int argc, char *argv[]) {
 
   const char *host = argv[1];
   int port = std::stoi(argv[2]);
+  
   if (!connect_to_server(host, port))
     return -1;
+  
   char folder[512] = {0};
   std::cout << "Specify folder to list: ";
   std::cin >> folder;
